28ae159fd5b4d062851bfab2794eea903e1454bf
Graemofo <dragonsleftshoe@gmail.com>
2019-03-30 15:18:44 +0000

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 #action mailer shit below
 host = 'whispering-anchorage-97578.herokuapp.com'
 config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
